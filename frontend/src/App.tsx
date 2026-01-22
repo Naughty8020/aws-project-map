1a811e9dceeb7b55903688ecb4b1de0ade811189
naughty8020 <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2026-01-22 15:45:52 +0900

    fix(graph):Adjust position of crowd graph on map page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,13 +14,25 @@ function App() {
   if (isLoading) return <div>読み込み中...</div>;
   if (error)return <div>エラーが発生しました</div>;
 
-  return (
+ return (
     <div>
       <Header />
-      <GoogleMap />
+
+<div className="flex gap-20 p-10 ml-10 mt-15">
+  {/* Map */}
+  <div className="flex-[2] min-w-[400px]">
+    <GoogleMap />
+  </div>
+
+  {/* Graph */}
+  <div className="flex-[2] flex justify-center">
+    <div className="w-full mt-40 max-w-full"> {/* max-w-md を外す */}
       <Apexcarts />
-      <h1>Hono + TanStack Query</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  </div>
+</div>
+      {/* <h1>Hono + TanStack Query</h1> */}
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
     </div>
   );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -41,7 +41,7 @@ export default function Example() {
 
   return (
     <header className="bg-gray-900">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+      <nav aria-label="Global" className="mx-auto flex max-w-1xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
               <div className="flex items-center space-x-2">
