d32970aadde4c3167cc2c7bf977afef13dcc9933
naughty8020 <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2026-02-06 01:57:38 +0900

    add(routes):tanstack router

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export const Route = createRootRoute({
+  component: () => (
+    <div className="min-h-screen ">
+      <Header />
+      <main>
+        <Outlet /> {/* ここに /weather の中身が表示される */}
+      </main>
+      <Footer />
+    </div>
+  ),
+})
